5c09f1f7c909c8b237982671d32e06cc731f604e
s1000779 <s1000779@WS1000779.bc.local>
2012-12-05 15:14:07 +0200

    Olen paras

---
+++ src/Tamaonpaketti/Moi.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tamaonpaketti;
+
+/**
+ *
+ * @author s1000779
+ */
+public class Moi {
+    public static void main(String[] args) {
+        System.out.println("Moi");
+    }
+}
